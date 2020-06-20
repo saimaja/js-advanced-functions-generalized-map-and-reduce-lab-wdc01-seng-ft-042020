77858b41fea8e373aa4d56cffe5f65a2606b469f
saimaja <saimaakhtar@gmail.com>
2020-06-20 01:54:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(srcArr, cb) {
+    let arr = []
+
+    for(let i=0; i<srcArr.length; i++){
+        let index = srcArr[i]
+        arr.push(cb(index))
+    }
+    return arr
+}
+
+function reduce(srcArr, cb, starting) {
+    let arr = (!!starting) ? starting : srcArr[0]
+    let i = (!!starting) ? 0 : 1
+
+    for(; i<srcArr.length; i++){
+        arr = cb(srcArr[i], arr)
+    }
+    return arr
+}
